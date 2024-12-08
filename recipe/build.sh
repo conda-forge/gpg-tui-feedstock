844d61bb989a9a82c7b45e7e202dcc1081bb2df8
Daniel Nachun <daniel.nachun@gmail.com>
2024-12-08 00:33:53 -0800

    add platforms

--- recipe/build.sh
+++ recipe/build.sh
@@ -2,6 +2,9 @@
 
 set -o xtrace -o nounset -o pipefail -o errexit
 
+export CARGO_PROFILE_RELEASE_STRIP=symbols
+export CARGO_PROFILE_RELEASE_LTO=fat
+
 # check licenses
 cargo-bundle-licenses \
     --format yaml \
@@ -18,6 +21,3 @@ mkdir -p ${PREFIX}/share/zsh/site-functions
 install -m 644 ${PKG_NAME}.bash ${PREFIX}/etc/bash_completion.d/${PKG_NAME}
 install -m 644 ${PKG_NAME}.fish ${PREFIX}/share/fish/vendor_completions.d/${PKG_NAME}.fish
 install -m 644 _${PKG_NAME} ${PREFIX}/share/zsh/site-functions/_${PKG_NAME}
-
-# strip debug symbols
-"$STRIP" "$PREFIX/bin/${PKG_NAME}"
