4721b38d5b632f5aa76de8fbb4393b146a70fb74
Daniel Nachun <daniel.nachun@gmail.com>
2025-12-11 17:11:58 -0800

    fix cross-compile

--- recipe/build.sh
+++ recipe/build.sh
@@ -14,11 +14,13 @@ cargo-bundle-licenses \
 # build statically linked binary with Rust
 cargo install --bins --no-track --locked --root ${PREFIX} --path .
 
-export OUT_DIR=${SRC_DIR}
-gpg-tui-completions
-mkdir -p ${PREFIX}/etc/bash_completion.d
-mkdir -p ${PREFIX}/share/fish/vendor_completions.d
-mkdir -p ${PREFIX}/share/zsh/site-functions
-install -m 644 ${PKG_NAME}.bash ${PREFIX}/etc/bash_completion.d/${PKG_NAME}
-install -m 644 ${PKG_NAME}.fish ${PREFIX}/share/fish/vendor_completions.d/${PKG_NAME}.fish
-install -m 644 _${PKG_NAME} ${PREFIX}/share/zsh/site-functions/_${PKG_NAME}
+if [[ ${build_platform} == ${target_platform} ]]; then
+    export OUT_DIR=${SRC_DIR}
+    gpg-tui-completions
+    mkdir -p ${PREFIX}/etc/bash_completion.d
+    mkdir -p ${PREFIX}/share/fish/vendor_completions.d
+    mkdir -p ${PREFIX}/share/zsh/site-functions
+    install -m 644 ${PKG_NAME}.bash ${PREFIX}/etc/bash_completion.d/${PKG_NAME}
+    install -m 644 ${PKG_NAME}.fish ${PREFIX}/share/fish/vendor_completions.d/${PKG_NAME}.fish
+    install -m 644 _${PKG_NAME} ${PREFIX}/share/zsh/site-functions/_${PKG_NAME}
+fi
